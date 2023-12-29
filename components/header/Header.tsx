8ba425216a2bd029227d8e95cf30296b38085a55
Giannidluca93 <gdluca93@gmail.com>
2023-12-29 19:44:06 -0300

    create header

---
+++ components/header/Header.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../navbar/NavBar";
+import Socials from "../socials/Socials";
+
+const Header = () => {
+  return (
+    <div>
+      <NavBar />
+      <Socials />
+    </div>
+  );
+};
+
+export default Header;
