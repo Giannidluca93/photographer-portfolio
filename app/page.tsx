65d49a188b8a19bb5c41c1805fc2314a8bb37272
Giannidluca93 <gdluca93@gmail.com>
2023-12-29 21:48:47 -0300

    import images gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import ImagesGallery from '@/components/imagesGallery/ImagesGallery';
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <ImagesGallery />
+    </div>
+  );
 }
