8323d67a325fdf23768ef2384f75ec113bf5a578
Giannidluca93 <gdluca93@gmail.com>
2023-12-30 12:28:45 -0300

    add link to heading

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const NavBar = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="p-8">
-        <h1 className="text-xs md:text-lg uppercase">Photographer</h1>
+        <h1 className="text-xs md:text-lg uppercase">
+          <Link href="/">Photographer</Link>
+        </h1>
       </div>
     </div>
   );
